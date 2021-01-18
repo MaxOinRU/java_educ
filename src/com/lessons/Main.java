601561ba071b8f902401cca736040689ef75610f
Max <JMaximusX@ya.ru>
2021-01-18 15:49:01 +0300

    Homework BEGIN

--- src/com/lessons/Main.java
+++ src/com/lessons/Main.java
@@ -3,6 +3,12 @@ package com.lessons;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("JAVA IS MY LIFE! or not...");
+	System.out.println("Rate 5, please (⌒‿⌒)/");
     }
 }
+//Команды для выполнения домашнего задания:
+//git add -A
+//
+//
+//
+//
--- src/com/lessons/reg/Reg.java
+++
@@ -1,4 +0,0 @@
-package com.lessons.reg;
-
-public class Reg {
-}
